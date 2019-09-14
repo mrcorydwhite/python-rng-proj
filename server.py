d0facd7c46ddc86e094b5cbed85fbcac983e8f35
Cory White <mrcorydwhite@gmail.com>
2019-09-14 19:15:10 +0000

    should now contain dependencies

--- server.py
+++ server.py
@@ -4,12 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 
-Def index():
-
-rn= random.randint(1,1000000)
-
-return str(rn)
+def index():
+    rn = random.randint(1,1000000)
+    return str(rn)
 
 if __name__ == "__main__":
-
-app.run(host = '0.0.0.0',port=80)
+    app.run(host = '0.0.0.0',port=80)
