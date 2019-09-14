2b380f8b23584d9ed2860958963f5f922e84ff34
mrcorydwhite <47005074+mrcorydwhite@users.noreply.github.com>
2019-09-14 14:06:48 -0500

    Update server.py

---
+++ server.py
@@ -0,0 +1,15 @@
+import random
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+
+Def index():
+
+rn= random.randint(1,1000000)
+
+return str(rn)
+
+if __name__ == "__main__":
+
+app.run(host = '0.0.0.0',port=80)
